951a88a914e1f4fbbb12a7b73e0cf70fdbedafa3
TianYong-Goh <66197002+TianYong-Goh@users.noreply.github.com>
2022-04-06 03:27:53 +0800

    search recent  tweets

--- tweetRetriever/tweetscraper.py
+++ tweetRetriever/tweetscraper.py
@@ -5,23 +5,38 @@ bearer_token = ""
 client = tweepy.Client(bearer_token)
 
 
-def get_tweets(username, max_num):
-    user = client.get_user(username=username)
-    user_id = user.data.id
-    tweets = client.get_users_tweets(id=user_id, max_results=max_num)
-
+def clean_tweets(tweet_arr):
     text = []
-    for t in tweets.data:
-        cleaned_text = clean_tweets(t.text)
+    for t in tweet_arr:
+        cleaned_text = clean_tweet(t.text)
         text.append(cleaned_text)
 
     return text
 
 
-def clean_tweets(tweet_text):
+def clean_tweet(tweet_text):
     tweet_text = re.sub(r"@[A-Za-z0-9]", "", tweet_text)
     tweet_text = re.sub(r"#", "", tweet_text)
     tweet_text = re.sub(r"RT[\s]", "", tweet_text)
     tweet_text = re.sub(r"https?:\/\/\S+", "", tweet_text)
 
     return tweet_text
+
+
+def get_tweets(username, max_num):
+    user = client.get_user(username=username)
+    user_id = user.data.id
+    tweets = client.get_users_tweets(id=user_id, max_results=max_num)
+
+    return clean_tweets(tweets.data)
+
+
+def search_tweets(query, max_no_result):
+    if max_no_result < 10:
+        max_no_result = 10
+    elif max_no_result > 100:
+        max_no_result = 100
+
+    tweets = client.search_recent_tweets(query=query, max_results=max_no_result);
+    return clean_tweets(tweets.data)
+
